3e4a6b6d33fbc79a9d376e81bc48f4bc1d9da481
walter erquinigo <a20012251@gmail.com>
2018-10-11 22:51:49 -0700

    Add project files.

---
+++ Scriabin/NotePlayer.fs
@@ -0,0 +1,21 @@
+﻿module Scriabin.NotePlayer
+
+open Xamarin.Essentials
+open System
+open Scriabin.Types
+
+let settings = 
+    let set = new SpeakSettings()
+    set.Pitch <- Nullable 0.5f
+    set
+
+let play (rand: Random) =
+    let simpleNote = 
+        let index = rand.Next(Counts.simpleNoteCount)
+        Enum.ToObject(typeof<SimpleNote>, index) |> unbox<SimpleNote>
+    let modifier = 
+        let index = rand.Next(Counts.modifierCount)
+        Enum.ToObject(typeof<Modifier>, index) |> unbox<Modifier>
+
+    let modifierName = Modifier.toString modifier
+    TextToSpeech.SpeakAsync(simpleNote.ToString() + " " + modifierName, settings) |> ignore
---
+++ Scriabin/Types.fs
@@ -0,0 +1,32 @@
+﻿[<AutoOpen>]
+module Scriabin.Types
+
+open System
+
+type SimpleNote =
+    | A = 0
+    | B = 1
+    | C = 2
+    | D = 3
+    | E = 4 
+    | F = 5
+    | G = 6
+
+type Modifier =
+    | Natural = 0
+    | Flat = 1
+    | Sharp = 2
+
+type Note = SimpleNote * Modifier
+
+type Interval = Note * Note
+
+module Counts =
+    let simpleNoteCount = Enum.GetValues(typeof<SimpleNote>).Length
+    let modifierCount = Enum.GetValues(typeof<Modifier>).Length
+
+module Modifier =
+    let toString (modifier: Modifier) =
+        match modifier.ToString() with
+        | "Natural" -> ""
+        | s -> s
